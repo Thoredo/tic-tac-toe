2af317b7ecf683077c3be6ca2b6cda31bcbde72a
Jurgen Stegeman <jurgenstegeman@live.nl>
2023-12-29 11:57:22 +0100

    Modify new game button

--- game.py
+++ game.py
@@ -10,7 +10,16 @@ class TicTacToeApp:
         self.master.welcome_label = tk.Label(
             text="Welcome to Tic Tac Toe!", font=("Helvetica", 26), fg="#D21404"
         )
-        self.master.welcome_label.grid(row=0, column=0, pady=(300, 10))
+        self.master.welcome_label.grid(row=0, column=0, pady=(300, 10), padx=200)
 
-        self.master.new_game_button = tk.Button(text="New Game")
-        self.master.new_game_button.grid(row=1, column=0, padx=375)
+        self.master.new_game_button = tk.Button(
+            text="New Game",
+            bg="green3",
+            fg="white",
+            font=("Helvetica", 18, "bold"),
+            command=self.new_game,
+        )
+        self.master.new_game_button.grid(row=1, column=0)
+
+    def new_game(self):
+        self.new_game_window = tk.Toplevel()
